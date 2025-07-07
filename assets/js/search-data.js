1bde5ef11f4d5cd6e81fed74fec14beadbf317b6
aregla <68516277+aregla@users.noreply.github.com>
2025-07-07 06:57:59 +0000

    Deploying to gh-pages from @ aregla/aregla.github.io@3bb299649730cb3c8b05dbdf9cdd56d99503b31f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-will-interning-at-tiktok-this-summer-as-a-user-experience-researcher",
+          title: 'I will interning at TikTok this summer as a User Experience Researcher 🥁...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
